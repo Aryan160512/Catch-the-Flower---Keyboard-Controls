ba06bb854f27a4d415ced29877b13f1655687fce
Aryan160512 <157271425+Aryan160512@users.noreply.github.com>
2024-12-22 10:50:40 +0000

    Add files via upload

---
+++ CW - Keyboard Controls.py
@@ -0,0 +1,57 @@
+import pgzrun
+import random
+
+score = 0
+
+WIDTH = 800
+HEIGHT = 450
+TITLE = 'Bee Game'
+
+bee = Actor('bee')
+bee.pos = (100, 100)
+
+flower = Actor('flower')
+flower.pos = (200, 400)
+
+isGameOver = False
+
+def flowerPos():
+    flower.pos = (random.randint(50, 400), random.randint(50, 400))
+
+def update():
+    global score
+    
+    if keyboard.left:
+        bee.x -= 5
+    elif keyboard.right:
+        bee.x += 5
+    elif keyboard.up:
+        bee.y -= 5
+    elif keyboard.down:
+        bee.y += 5
+    
+    isCollided = bee.colliderect(flower)
+    #print(isCollided)
+
+    if isCollided:
+        flowerPos()
+        score += 1
+
+def endGame():
+    global isGameOver
+    isGameOver = True
+
+def draw():
+    screen.blit('background', (0, 0))
+
+    bee.draw()
+    flower.draw()
+
+    screen.draw.text(f'Score: {score}', (50, 50))
+
+    if isGameOver:
+        screen.fill('black')
+        screen.draw.text(f'GAME OVER, YOUR SCORE IS {score}', (300, 225))
+
+clock.schedule(endGame, 60)
+pgzrun.go()
